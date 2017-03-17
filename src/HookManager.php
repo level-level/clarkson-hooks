bee687abb4aa75623027e86f3cba3d1e96c57a4b
Niels de Blaauw <niels@level-level.com>
2017-03-17 14:49:26 +0100

    Renamed entry file, only register hooks once, timing info added

--- src/HookManager.php
+++ src/HookManager.php
@@ -2,10 +2,57 @@
 namespace Clarkson\Hooks;
 
 class HookManager{
+  /**
+   * Contains timing info for the first instance a hook was called.
+   */
+  public static $timings = array();
+
+  /**
+   * Starttime of profile.
+   */
+  public static $starttime = null;
+
+  public function __construct(){
+    self::$starttime = microtime(true);
+  }
+
   public function loadHook($name){
     $hook = '\\Hooks\\'.str_replace('/', '\\',$name);
-    if(class_exists($hook)){
-      $hook::register_hooks($name);
+    if(!isset(self::$timings[$name])){
+      self::$timings[$name] = microtime(true);
+      if(class_exists($hook)){
+        $hook::register_hooks($name);
+      }
     }
+  }
+
+  public static function get_timings(){
+    $timings = array();
+    foreach(self::$timings as $name => $timing){
+      $timings[] = array('name'=>$name, 'delta'=>($timing - self::$starttime));
+    }
+    usort($timings, function($a,$b){
+      if($a['delta'] == $b['delta']){
+        return 0;
+      }
+      if($a['delta'] > $b['delta']){
+        return 1;
+      }
+      return -1;
+    });
+    echo "<table>";
+    $last = 0;
+    foreach($timings as $timing){
+      echo "<tr>";
+      echo "<td>{$timing['name']}</td>";
+      $display_delta = number_format($timing['delta'], 4);
+      echo "<td>{$display_delta} seconds</td>";
+      $diff = number_format(($timing['delta'] - $last) * 1000, 0);
+      $last = $timing['delta'];
+      echo "<td>{$diff} ms</td>";
+      echo "</tr>";
+    }
+    echo "</table>";
+
   }
 }
