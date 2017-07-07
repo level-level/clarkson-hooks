fbc81e99731c18eb55da5911ff1ecd73ccfa7d54
Niels de Blaauw <niels.de.blaauw@gmail.com>
2017-07-07 10:53:12 +0200

    Remove error when not in WP context
    Some tools (such as [php-composter](https://github.com/php-composter/php-composter)) load the `vendor/autoload.php` from the command line. In this context WP is not available and the previous version would crash on an `undefined function add_action` or similar. The WP context is not needed to do static checking such as phpMD or phpCS.

--- clarkson-hooks.php
+++ clarkson-hooks.php
@@ -1,4 +1,10 @@
 <?php
 use Clarkson\Hooks\HookManager;
+
+// Skip loading the hookmanager if not in a WordPress context
+if ( ! defined('WPINC') ) {
+  return;
+}
+
 $hookManager = new HookManager();
 add_action('all', array($hookManager, 'loadHook'));
